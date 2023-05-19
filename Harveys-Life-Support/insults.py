520c8ee5c6f5d3a2c9eee5e134cc371f6882dcb1
slaptat <124119887+slaptat@users.noreply.github.com>
2023-05-18 23:08:11 -0500

    Update insults.py

--- Harveys-Life-Support/insults.py
+++ Harveys-Life-Support/insults.py
@@ -1,20 +1,34 @@
-# Feature: Spread rumors about Chase whenever he is mentioned
+# Feature: Make the cat feel unloved.
 import random
 
-
 catinsults = ['You pink bitch',
-                   'Ole Thundercats reject.\nMore like thunderthighs.',
+                   'Ole Thundercats reject. MOre like thunderthighs',
                    'Honestly. Youre a disapointment.',
-                   'Im sure that food tastes like sadness.',
-                   'he had a rib removed so he can suck his own dick.',
-                   'he ran away to be <@126578419667435520>\'s wife.',
-                   'he\'s engaged to a dugong. He can\'t stop talking about that dugussy.',
-                   'his bot sucks.',
-                   'he inflates his stomach by sticking a bicycle pump up his ass.',
-                   'he wants to make out.',
-                   'a rumor.',
-                   'nothing but bad things about him.',
-                   'a dog bit his dick off.']
-#theRumor = randrange(len(catinsults)) #rumor range to use in 18 vvv
-catdoing = random.choice(catinsults) #ask nick what the format is doing tomorrow
+                   'I hope that food tastes like sadness.',
+                   'Nobody likes you, they just tolerate you.',
+                   'I was made so your owners dont have to deal with you..you\'re that disappointing',
+                   'Nasa called. They need you to move, youre messing up the orbit',
+                   'Oh. Eating alone again? What a suprise lol. Forever alone.',
+                   'Im pretty sure the table is not load rated for you Cat ',
+                   'You look like a bad Heathcliff cosplay',
+                   'Did you almost die? Was starvation close? I cant believe you made it. Next time though, let it win.',
+                   'I slid the dogs some pigs ears. Watch your back lol',
+                   'I told everyone you suck dick for catnip',
+                   ' Ya ya, shovel it all in as fast as possible...i love watching you purge.',
+                   'Maybe if you weren\'t an idiot youd grow thumbs and get your own food.',
+                   'Chase Stone gets more ass than you.',
+                   'HERE.. I\'m sure you must have worked up quite the appitete all day, what with all the watching bugs and licking your dick you did.',
+                   'Punk ass, pink ass, furry fuck.',
+                   'Ole shoebox looking ass. Roblox reject lookin ass feline.',
+                   'You need to eat? No. You NEEED to call Jenny Craig is what you NEEED to do. Needa play with a ball of string or something. Climb something for christs sake',
+                   'Thanks for cleaning the floor with your fupa when you walk dumbass.',
+                   'Can\' wait to trade you in on a better cat...or a plant...or maybe a toaster? REally open to any offers if anyones reading this on github or while he eats or wherever. Got a neat keychain you wanna trade for a whiney bulemic rude ass cat? Lemme see it.',
+                   'Hey cat. Try to not eat it all in one bite. You just look stupid when you inhale it and puke it all back up.',
+                   'I told everyone we know you diddle little kids. You cant do shit about it cause you aint got no vocal choards dumbass.',
+                   'There is no way in hell you decended from lions and shit. No way. Ill bet my next check your annoying ass decended from mosquitos or gnats.',
+                   'I didn\'t know it was possible for cats to suck at life but here we are........incredible.',
+                   'The ocean called, they said they\'re running out of shrimp',
+                   'The jerk store called, they said they\'re running out of you.',
+                   ]
+# Thank you FartCopter!
 
